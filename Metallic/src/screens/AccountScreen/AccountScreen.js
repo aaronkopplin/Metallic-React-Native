6dea6e1efb205d3b2f176e251c3f313705e76b6c
PerpetualLink <Samack1011@gmail.com>
2021-03-01 11:46:42 -0500

    Finally solved Issue with getting information from database.

--- Metallic/src/screens/AccountScreen/AccountScreen.js
+++ Metallic/src/screens/AccountScreen/AccountScreen.js
@@ -20,26 +20,9 @@ import CustomButton from "../../../button";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { masterStyles } from "../../../masterStyles";
 
-// var fullName;
-
-async function getUser(datab, userID) {
-    var users = datab.collection('users');
-    const snapshot = await users.where('id', '==', userID).get();
-
-    if (snapshot.empty) {
-        alert('no matching');
-        return;
-    }
-    snapshot.forEach(doc => {
-        alert(doc.id, '=>', doc.data());
-        // alert(doc.data().fullName)
-        return doc.data().fullName;
-    });
-    // return snapshot.doc.data().fullName;
-}
-
 export function AccountScreen(props) {
-
+    const [userFullName, setFullName] = useState("");
+    const [userEmail, setEmail] = useState("");
     const onLogoutPress = () => {
         console.log("Logout Pressed.");
         firebase
@@ -55,29 +38,24 @@ export function AccountScreen(props) {
     // var database = firebase.database();
     var user = firebase.auth().currentUser;
     var db = firebase.firestore();
-    
-    var fullName, email, uid;
-    var info;
-
-    
-
+    var uid;    
     if (user != null) {
-        // fullName = user.displayName
-        email = user.email;
         uid = user.uid;
-        // fullName = () => {
-        //     firebase.auth().then(() => {
-        //         getUser(db, uid);
-        //     }).catch((error) => {
-        //         console.log(error);
-        //     });
-        // }
-        // getUser(db, uid).then(toString());
-        // getUser(db, uid).then(( => {
-        //     fullName
-        // }))
-        alert(getUser(db, uid).then(toString()));
+        async function getUser(datab, userID) {
+            var users = datab.collection('users');
+            const snapshot = await users.where('id', '==', userID).get();
         
+            if (snapshot.empty) {
+                alert('no matching');
+                return;
+            }
+            snapshot.forEach(doc => {
+                setFullName(doc.data().fullName);
+                setEmail(doc.data().email);
+                return doc;
+            });
+        }
+        getUser(db,uid);
     }
 
     return (
@@ -87,8 +65,8 @@ export function AccountScreen(props) {
                 <CustomButton onPress={onLogoutPress} text='Logout' height={50}>
                 </CustomButton>
             </View>
-            <Text style={{paddingTop: 40}}> {fullName} </Text>
-            <Text style={{paddingTop: 20}}> {email} </Text>
+            <Text style={{paddingTop: 40}}> {userFullName} </Text>
+            <Text style={{paddingTop: 20}}> {userEmail} </Text>
             
         </View>
         // </SafeAreaView>
