edc02060f2feadf2dc5b702e343cd4586d59393b
aaron <kopplina@mail.gvsu.edu>
2021-03-03 18:20:23 -0500

    adding account data and persistent storage

--- Metallic/src/screens/UserSearchScreen/UserSearchScreen.js
+++ Metallic/src/screens/UserSearchScreen/UserSearchScreen.js
@@ -32,82 +32,46 @@ export function UserSearchScreen(props) {
     
 
     const [users, setUsers] = useState([]);
-    const [thisUserEmail, setThisUserEmail] = useState("");
-    const [thisUserName, setThisUserName] = useState("");
-    const [thisUserID, setThisUserID] = useState("");
     const userRef = firebase.firestore().collection("users");
 
-    // async function updateFoundUsers() {
-    //     // setSearchText();
-    //     var users = firebase.firestore().collection('users');
-    //     const snapshot = await users.get();
-    //     setUsers();
-    //     const newEntities = [];
-
-    //     if (snapshot.empty) {
-    //         alert('no matching');
-    //         return;
-    //     }
-    //     // alert(searchText);
-    //     snapshot.forEach(doc => {
-    //         setThisUserEmail(doc.data().email);
-    //         setThisUserName(doc.data().fullName);
-    //         setThisUserID(doc.data().id);
-
-    //         const entity = doc.data();
-    //         entity.id = doc.id;
-    //         entity.text = thisUserEmail;
-    //         entity.fullName = doc.data().fullName;
-    //         entity.email = doc.data().email;
-    //         // newEntities.push(entity);
-    //         if (searchText.length > 0) {
-    //             if (thisUserEmail.includes(searchText)) {
-    //                 // alert("True");
-    //                 alert(entity.email);
-    //                 newEntities.push(entity);
-    //             }
-    //         }
-    //         return doc;
-            
-    //     });
-    //     setUsers(newEntities);
-    //         // setFullName(doc.data().fullName);
-    //         // setEmail(doc.data().email);
-    //         // return doc;
-    // }
-    
     useEffect(() => {
         userRef
-            // .orderBy("createdAt", "desc")
+            
             .onSnapshot(
                 (querySnapshot) => {
-                    const newEntities = [];
-                    querySnapshot.forEach(doc => {
-                        const entity = doc.data();
-                        // setThisUserID(entity.id);
-                        // setThisUserEmail(entity.email);
-                        entity.id = doc.id;
-                        entity.text = doc.data().email;
-                        entity.fullName = doc.data().fullName;
-                        entity.userName = doc.data().userName;
-                        setThisUserID(entity.id);
-                        setThisUserEmail(entity.email);
-                        
-                        newEntities.push(entity);
-                        // if (searchText.trim() != "") {
-                        //     alert(thisUserEmail);
-                        //     if (thisUserEmail.includes(searchText)) {
-                        //         alert(thisUserEmail);
-                        //         console.error("true");
-                        //         console.log('true');
-                        //         newEntities.push(entity);
-                                 
-                        //     }
-                        // }
-                        
-                        
-                    });
-                    setUsers(newEntities);
+                    var newEntities = [];
+                    if (searchText.trim != "") {
+                        querySnapshot.forEach(doc => {
+                            const entity = doc.data();
+
+                            entity.id = doc.id;
+                            entity.email = doc.data().email;
+                            entity.fullName = doc.data().fullName;
+                            entity.userName = doc.data().userName;
+
+                            // get text prior to @ of email
+                            var emailToSearch = String(entity.email).substring(0, (String(entity.email).lastIndexOf('@')));
+
+                            if (emailToSearch.includes(searchText) || String(entity.fullName).includes(searchText) || String(entity.userName).includes(searchText)) {
+                                newEntities.push(entity);
+                            }
+                            
+                        });
+                        setUsers(newEntities);
+                    } else {
+                        querySnapshot.forEach(doc => {
+                            const entity = doc.data();
+
+                            entity.id = doc.id;
+                            entity.fullName = doc.data().fullName;
+                            entity.userName = doc.data().userName;
+                            
+                            newEntities.push(entity);
+                        });
+                        setUsers(newEntities);
+                    }
+                    
+                    
                 },
                 (error) => {
                     console.log(error);
@@ -115,9 +79,6 @@ export function UserSearchScreen(props) {
             );
     }, []);
 
-    // getListViewItem = (user) => {
-    //     Alert.alert(user.key);
-    // }
     const navigation = useNavigation();
     const renderUser = ({ item, index }) => {
         return (
@@ -129,10 +90,17 @@ export function UserSearchScreen(props) {
 
                 });
                 }}>
-                    <Text style={masterStyles.headingsSmallNotBold} >
-                    {item.text}
-                    
-                    </Text>
+                    <View>
+                        <Text>
+                        <View style={{justifyContent: "center", flex: 1}}>
+                            <Image style={{height: 35, width: 20}} source={require("../../../assets/Default_Img.png")}></Image>
+                        </View> 
+                        <View style={{justifyContent: "center", flex: 1}}>
+                            <Text style={[masterStyles.headingsSmall]} >{'\t'}{item.email}</Text>
+                            <Text style={[masterStyles.headingsSmallNotBold]} >{'\t'}{item.userName}</Text>
+                        </View>
+                        </Text>
+                    </View>
                 </TouchableOpacity>
                 
             </View>
@@ -140,43 +108,35 @@ export function UserSearchScreen(props) {
     };
 
     return (
-        <SafeAreaView style={{flex: 1, backgroundColor: "#1e1c21", alignContent: 'center', justifyContent: 'space-evenly'}}>
-            <View style={{backgroundColor: "#2e2b30", alignItems: 'center', justifyContent: 'space-evenly', borderRadius: 4, width: screenSize.width - 20, left: 10}}>
-                <View style={{backgroundColor: '#2e2b30', alignItems: 'center', height: screenSize.height * 0.01, paddingBottom: 20}} />
-                <View style={{width: screenSize.width - 60}}>
+        <SafeAreaView style={{flex: 1, backgroundColor: "#1e1c21", alignContent: 'center', justifyContent: 'space-evenly', alignItems: 'center'}}>
+            <View style={{backgroundColor: "#2e2b30", alignItems: 'center', paddingBottom: 40, borderRadius: 4, height: screenSize.height * 0.75, width: screenSize.width - 20}}>
+                <View style={{backgroundColor: '#2e2b30', alignItems: 'center', top: 20, height: screenSize.height * 0.01, paddingBottom: 20}} />
+                <View style={{width: screenSize.width - 40}}>
                     <TextInput
                         style={[masterStyles.input]}
                         placeholder="Enter name/username to search for a user"
                         placeholderTextColor="#aaaaaa"
-                        // onChangeText={[text => setSearchText(text)]}
+                        onChangeText={text => setSearchText(text)}
+                        clearButtonMode="while-editing"
                         underlineColorAndroid="transparent"
                         autoCapitalize="none"
                     />
                 </View>
-                {/* <View style={{paddingTop: 15}}>
-                    <CustomButton
-                        text="search"
-                        color="#1e1c21"
-                        width={screenSize.width - 60}
-                        height={screenSize.height / 20}
-                        paddingTop={20}
-                        // onPress={[alert(searchText)]}
-                    />
-                </View> */}
-                <View style={{paddingTop: 20, paddingBottom: 20, width: screenSize.width - 60}}>
+                <View style={{paddingVertical: 20, height: (screenSize.height * 0.75) - 40, width: screenSize.width - 20, paddingHorizontal: 10}}>
                     <FlatList
                         data={users}
                         renderItem={renderUser}
                         keyExtractor={(item) => item.id}
                         removeClippedSubviews={true}
+                        // style={{height: screenSize.height * 0.6}}
                     />
                     
                 </View>
 
-                <View style={
+                {/* <View style={
                     masterStyles.mainBackground,
                     {flex: 1}
-                }/>
+                }/> */}
 
             </View>
         </SafeAreaView>
