f916e7f3f09228d123813affd1a89cc051ef9a3f
Myren Mitchell <mitchmad@mail.gvsu.edu>
2021-03-09 18:39:06 -0500

    style updates to recent chats

--- Metallic/src/screens/RecentChatsScreen/RecentChatsScreen.js
+++ Metallic/src/screens/RecentChatsScreen/RecentChatsScreen.js
@@ -15,16 +15,38 @@ import CustomButton from "../../../button";
 import { masterStyles } from '../../../../Metallic/masterStyles';
 
 export function RecentChatsScreen({ navigation }) {
+
+    const screenSize = Platform.OS === "web" ? Dimensions.get("window") : Dimensions.get("screen");
+
     return (
         <View style={masterStyles.mainBackground}>
+            <View style={
+                masterStyles.mainBackground,
+                {flex: 0.1}
+            }></View>
+            <View
+                style={{
+                    flex: 3,
+                    backgroundColor: "#2e2b30",
+                    width: screenSize.width - 40,
+                    height: Platform.OS === "web" ? screenSize.height/2.5 : screenSize.width - 30,
+                    paddingTop: screenSize.height / 50,
+                    paddingLeft: 20,
+                    borderRadius: 4,
+                }}
+            >
             <TouchableOpacity
                 onPress={() => {
                     navigation.navigate("Payments");
                 }}
             >
-                <Text style={{color: masterStyles.headings.color}}> Go To Payments Screen </Text>
+                <Text style={{color:'white'}}> Go To Payments Screen </Text>
             </TouchableOpacity>
-            <Text style={{color: masterStyles.headings.color}}> Hello recent chats </Text>
+            <View style={
+                masterStyles.mainBackground,
+                {flex: 0.5}
+            }></View>
+            </View>
         </View>
     );
 }
