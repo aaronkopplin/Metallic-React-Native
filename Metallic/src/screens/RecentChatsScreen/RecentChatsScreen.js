17f60fbb70f4bb348a16b0a61b256d11acec4159
hubbard008 <hubbard018@yahoo.com>
2021-03-24 17:09:08 -0400

    workaround for navigating to payment screen from recent chats

--- Metallic/src/screens/RecentChatsScreen/RecentChatsScreen.js
+++ Metallic/src/screens/RecentChatsScreen/RecentChatsScreen.js
@@ -15,11 +15,18 @@ import CustomButton from "../../../button";
 import { masterStyles } from '../../../../Metallic/masterStyles';
 
 export function RecentChatsScreen({ navigation }) {
+    const currUser = firebase.auth().currentUser;
     return (
         <View style={masterStyles.mainBackground}>
             <TouchableOpacity
                 onPress={() => {
-                    navigation.navigate("Payments");
+                    navigation.navigate("Payments", {
+                        email: currUser.email,
+                        fullName: currUser.fullName,
+                        userName: currUser.userName,
+                        address: currUser.address
+                        //uid: uid
+                    });
                 }}
             >
                 <Text style={{color: masterStyles.headings.color}}> Go To Payments Screen </Text>
