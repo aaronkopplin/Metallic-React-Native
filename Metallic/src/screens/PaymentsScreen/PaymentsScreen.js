e0bcc7dc9c632bdb681c0ce3ab66bff62ef571bc
aaron <kopplina@mail.gvsu.edu>
2021-04-18 15:08:19 -0400

    clicking a users profile image will now navigate to the new account screen

--- Metallic/src/screens/PaymentsScreen/PaymentsScreen.js
+++ Metallic/src/screens/PaymentsScreen/PaymentsScreen.js
@@ -373,7 +373,7 @@ export function PaymentsScreen({ route }) {
             >
                 <TouchableOpacity
                     onPress={() => {
-                        navigation.navigate("UserAccountScreen", {
+                        navigation.navigate("ViewOtherAccount", {
                             email: email,
                             fullName: fullName,
                             userName: userName,
